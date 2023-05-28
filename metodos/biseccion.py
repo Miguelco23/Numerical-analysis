ab40d0490fc21bc6abae4a9ab7351e8b0002eb8c
Miguel C <miguel.cabrera@gestionpositiva.com>
2023-05-27 23:55:17 -0500

    Correccion de lectura de funcion en biseccion

--- metodos/biseccion.py
+++ metodos/biseccion.py
@@ -1,23 +1,26 @@
 def biseccion(f, a, b, tol):
 
-    if f(a)*f(b) >= 0:
+    # Evaluar la cadena de texto como una función
+    def evaluar_funcion(x):
+        return eval(f)
+
+    if evaluar_funcion(a) * evaluar_funcion(b) >= 0:
         print("Error: La función no cambia de signo en el intervalo dado.")
         return None
     else:
-        c = (a+b)/2
-        while abs(f(c)) > tol:
-            if f(a)*f(c) < 0:
+        c = (a + b) / 2
+        while abs(evaluar_funcion(c)) > tol:
+            if evaluar_funcion(a) * evaluar_funcion(c) < 0:
                 b = c
             else:
                 a = c
-            c = (a+b)/2
+            c = (a + b) / 2
         return c
 
-# F(x)
-def f(x):
-    return x**2 - 2
+# Cadena de texto que representa la función
+f = "x**2 - 2"
 
-# intervalo y toleracia
+# Intervalo y tolerancia
 a = 0
 b = 2
 tol = 1e-6
