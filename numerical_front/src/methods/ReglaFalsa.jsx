d3ed155a4ac75af50dda0c87ec59f019f7d7e43b
Miguel C <miguel.cabrera@gestionpositiva.com>
2023-05-29 16:56:17 -0500

    Regla falsa ready for API consuming

---
+++ numerical_front/src/methods/ReglaFalsa.jsx
@@ -0,0 +1,81 @@
+import React, { useState } from 'react';
+
+const ReglaFalsa = () => {
+  const [a, setA] = useState('');
+  const [b, setB] = useState('');
+  const [func, setFunc] = useState('');
+  const [tolerance, setTolerance] = useState('');
+  const [maxIter, setMaxIter] = useState('');
+  const [result, setResult] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (a === "" || b === "" || func === "" || tolerance === "" || maxIter === "") {
+      alert("Rellena todos los campos");
+    } else {
+      const data = {
+        a: parseFloat(a),
+        b: parseFloat(b),
+        func,
+        tolerance: parseFloat(tolerance),
+        maxIter: parseFloat(maxIter)
+      };
+
+      console.log(data);
+
+      // try {
+      //   const response = await fetch('API_URL', {
+      //     method: 'POST',
+      //     headers: {
+      //       'Content-Type': 'application/json',
+      //     },
+      //     body: JSON.stringify(data),
+      //   });
+
+      //   const resultData = await response.json();
+      //   setResult(resultData.result);
+      // } catch (error) {
+      //   console.error('Error:', error);
+      //   alert('Error:', error);
+      // }
+    }
+  };
+
+  return (
+    <div>
+      <h2>Regla Falsa</h2>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Intervalo [a]:
+          <input type="number" value={a} onChange={(e) => setA(e.target.value)} />
+        </label>
+        <br />
+        <label>
+          Intervalo [b]:
+          <input type="number" value={b} onChange={(e) => setB(e.target.value)} />
+        </label>
+        <br />
+        <label>
+          Funcion:
+          <input type="text" value={func} onChange={(e) => setFunc(e.target.value)} />
+        </label>
+        <br />
+        <label>
+          Tolerancia:
+          <input type="number" value={tolerance} onChange={(e) => setTolerance(e.target.value)} />
+        </label>
+        <br />
+        <label>
+          Iteracion maxima:
+          <input type="number" value={maxIter} onChange={(e) => setMaxIter(e.target.value)} />
+        </label>
+        <br />
+        <button type="submit">Calcular</button>
+      </form>
+      {result && <p>Resultado: {result}</p>}
+    </div>
+  );
+};
+
+export default ReglaFalsa;
