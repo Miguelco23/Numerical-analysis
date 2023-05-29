6c73d8a647760e9a3c9ad71d12cb11ab230e8d02
Miguel C <miguel.cabrera@gestionpositiva.com>
2023-05-29 17:22:17 -0500

    Fixing Secante for string function and returning result

--- metodos/secante.py
+++ metodos/secante.py
@@ -1,32 +1,29 @@
-def secante(f, x0, x1, tol, max_iter=100):
-    fx0 = f(x0)
-    fx1 = f(x1)
+def secante(f, x0, x1, tol, max_iter):
     i = 0
-    while abs(fx1) > tol and i < max_iter:
+    while i < max_iter:
         try:
-            dx = (x1 - x0)/(fx1 - fx0)
+            fx0 = eval(f.replace('x', str(x0)))
+            fx1 = eval(f.replace('x', str(x1)))
+            dx = (x1 - x0) / (fx1 - fx0)
             x0 = x1
             fx0 = fx1
-            x1 = x1 - fx1*dx
-            fx1 = f(x1)
+            x1 = x1 - fx1 * dx
+            fx1 = eval(f.replace('x', str(x1)))
+            if abs(fx1) <= tol:
+                return("La raíz de la función es: "+ str(x1))
             i += 1
         except ZeroDivisionError:
-            print("Error: División por cero.")
-            return None
-    if abs(fx1) > tol:
-        print("El método no converge después de %d iteraciones." % max_iter)
-        return None
-    return x1
+            return("Error: División por cero.")
+    return("El método no converge después de %d iteraciones." % max_iter)
 
-# F(x)
-def f(x):
-    return x**2 - 2
 
 # intervalo y toleracia
-x0 = 1
-x1 = 2
-tol = 1e-6
+# f = "x**2 - 2"
+# x0 = 1
+# x1 = 2
+# tol = 1e-6
+# max_iter = 5
 
-# raiz = secante(f, x0, x1, tol)
-# print("La raíz de la función es:", raiz)
+# raiz = secante(f, x0, x1, tol, max_iter)
+# print(raiz)
 
