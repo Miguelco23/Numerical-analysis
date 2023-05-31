7834498929aea15e66c81831e162cd5dc5a41302
Gustavo-Lopez13 <46933074+Gustavo-Lopez13@users.noreply.github.com>
2023-05-31 13:13:30 -0500

    Update Jacobi.py

--- Backend/metodos/Jacobi.py
+++ Backend/metodos/Jacobi.py
@@ -1,7 +1,10 @@
 import numpy as np
 
-def Jacobi(A, b, x0, tol, Nmax):
+def Jacobi(A_, b_, x0_, tol, Nmax):
     # Inicialización
+    A=np.array(A_)
+    b = np.array(b_)
+    x0 = np.array(x0_)
     D = np.diag(np.diag(A))
     L = -np.tril(A) + D
     U = -np.triu(A) + D
@@ -23,4 +26,4 @@ def Jacobi(A, b, x0, tol, Nmax):
     iterations = cont
     err = E
 
-    return x, iterations, err
+    return ({"x":x,"iterations":iterations,"Error":err})
