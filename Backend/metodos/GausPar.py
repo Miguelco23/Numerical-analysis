f5b1517a0f2d121c4e80f0b34fe98d2368d1f734
Gustavo-Lopez13 <46933074+Gustavo-Lopez13@users.noreply.github.com>
2023-06-03 15:30:41 -0500

    Update GausPar.py

--- Backend/metodos/GausPar.py
+++ Backend/metodos/GausPar.py
@@ -1,15 +1,15 @@
 import numpy as np
 
-def GausPar(A_, b_):
-    # Inicialización
-    A=np.array(A_)
+def GausPar(A_, b_, Err=1):
+    # Initialization
+    A = np.array(A_)
     b = np.array(b_)
     n = A.shape[0]
     M = np.concatenate((A, b.reshape(n, 1)), axis=1)
     
-    # Reducción del sistema
+    # System reduction
     for i in range(n-1):
-        # Cambio de filas
+        # Row swapping
         aux0, aux = np.max(np.abs(M[i+1:n, i])), np.argmax(np.abs(M[i+1:n, i]))
         if aux0 > np.abs(M[i, i]):
             aux2 = M[i+aux, i:n+1].copy()
@@ -18,15 +18,27 @@ def GausPar(A_, b_):
         
         for j in range(i+1, n):
             if M[j, i] != 0:
-                M[j, i:n+1] = M[j, i:n+1] - (M[j, i]/M[i, i]) * M[i, i:n+1]
+                if not np.isclose(M[i, i], 0):
+                    M[j, i:n+1] = M[j, i:n+1] - (M[j, i]/M[i, i]) * M[i, i:n+1]
     
-    # Entrega de resultados
-    x = SustitucionRegresiva(M)  # Sustitución regresiva
-    return np.array2string(x)
+    # Results delivery
+    x = SustitucionRegresiva(M)  # Backward substitution
+    
+    # Error calculation
+    if Err == 1:
+        error = np.abs(np.dot(A, x) - b)
+        return np.array2string(x), np.array2string(error)
+    elif Err == 2:
+        error = np.abs(np.dot(A, x) - b) / np.abs(b)
+        return np.array2string(x), np.array2string(error)
+    else:
+        return "Warning: Invalid value for parameter Err. It has been set to 1 (absolute error)."
+    
 
 def SustitucionRegresiva(M):
     n = M.shape[0]
     x = np.zeros(n)
     for i in range(n-1, -1, -1):
-        x[i] = (M[i, n] - np.dot(M[i, i+1:n], x[i+1:n])) / M[i, i]
+        if not np.isclose(M[i, i], 0):
+            x[i] = (M[i, n] - np.dot(M[i, i+1:n], x[i+1:n])) / M[i, i]
     return x
