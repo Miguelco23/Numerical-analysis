bb0dd4eb333827cec142272fe0838c36a8dc964b
Gustavo-Lopez13 <46933074+Gustavo-Lopez13@users.noreply.github.com>
2023-06-03 15:53:07 -0500

    Update GausSeidel.py

--- Backend/metodos/GausSeidel.py
+++ Backend/metodos/GausSeidel.py
@@ -1,8 +1,8 @@
 import numpy as np
 
-def GausSeidel(A_, b_, x0_, tol, Nmax):
+def GausSeidel(A_, b_, x0_, tol, Nmax, Err):
     # Inicialización
-    A=np.array(A_)
+    A = np.array(A_)
     b = np.array(b_)
     x0 = np.array(x0_)
     D = np.diag(np.diag(A))
@@ -17,13 +17,26 @@ def GausSeidel(A_, b_, x0_, tol, Nmax):
     # Ciclo
     while E > tol and cont < Nmax:
         xact = T @ xant + C
-        E = np.linalg.norm(xant - xact)
+        E_abs = np.linalg.norm(xant - xact)
+        E_rel = E_abs / np.linalg.norm(xact)
         xant = xact
+        if Err == 1:
+            E = E_abs
+        elif Err == 2:
+            E = E_rel
+        else:
+            return "Invalid value for Err. Please choose 1 for absolute error or 2 for relative error."
         cont += 1
 
     # Entrega de resultados
     x = xact
     iterations = cont
-    err = E
-
-    return ({"x":x,"iterations":iterations,"Error":err})
+    
+    if Err == 1:
+        error = E_abs
+        return {"x": x, "iterations": iterations, "absolute_error": error}
+    elif Err == 2:
+        error = E_rel
+        return {"x": x, "iterations": iterations, "relative_error": error}
+    else:
+        return "Invalid value for Err. Please choose 1 for absolute error or 2 for relative error."
