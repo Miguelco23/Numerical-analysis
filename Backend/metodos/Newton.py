affe08c6bf86b7c1491830cdb811670158b0482c
Gustavo-Lopez13 <46933074+Gustavo-Lopez13@users.noreply.github.com>
2023-06-03 17:59:28 -0500

    Update Newton.py

--- Backend/metodos/Newton.py
+++ Backend/metodos/Newton.py
@@ -1,41 +1,29 @@
-from math import *
-import numpy as np
-
-def Newton(f, derf, x0, tol, Nmax, Err):
+def RaicesMultiples(f, df, d2f, x0, tol, Nmax, Err):
+    # Inicialización
     xant = x0
-    flambda = lambda x: eval(f)
-    fant = flambda(xant)
+    fant = f(xant)
     E = 1000
     cont = 0
-    derflambda = lambda x: eval(derf)
+    errores = []  # Lista para almacenar los errores
     
-    error_abs = []  # Lista para almacenar los errores absolutos
-    error_rel = []  # Lista para almacenar los errores relativos
-    
+    # Ciclo
     while E > tol and cont < Nmax:
-        xact = xant - fant / (derflambda(xant))
-        fact = flambda(xact)
-        E = abs(xact - xant)  # Error absoluto
-        error_abs.append(E)
-
-        if xact != 0:
-            error_relativo = abs((xact - xant) / xact)  # Error relativo
-            error_rel.append(error_relativo)
-
+        xact = xant - (fant * df(xant)) / ((df(xant)) ** 2 - fant * d2f(xant))
+        fact = f(xact)
+        E = abs(xact - xant)
         cont = cont + 1
         xant = xact
         fant = fact
-
-    if E < tol:
-        x = xact
-        ite = cont
-        err = E
-        if Err == 1:
-          return ("The function has a root at ", x, " with absolute errors: ", error_abs)
-        elif Err == 2:
-          return ("The function has a root at ", x, " with relative errors: ", error_rel)
-        else:
-          return "Invalid value for the 'Err' parameter. Please use 1 for relative error or 2 for absolute error."
-
+        errores.append(E)  # Agregar el error a la lista
+        
+    # Entrega de resultados
+    x = xact
+    iteraciones = cont
+    
+    if Err == 1:  # Error absoluto
+        return {"x": x, "Iter": iteraciones, "Errores": errores}
+    elif Err == 2:  # Error relativo
+        errores_relativos = [e / abs(x) for e in errores]  # Calcular errores relativos
+        return {"x": x, "Iter": iteraciones, "Errores": errores_relativos}
     else:
-        return ("No solution exists")
+        return "Invalid value for the 'Err' parameter. Please use 1 for relative error or 2 for absolute error."
