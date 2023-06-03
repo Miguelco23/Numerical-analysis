6ffb0751ba7fad05de68bd93a6bc2b51b4b3c642
Gustavo-Lopez13 <46933074+Gustavo-Lopez13@users.noreply.github.com>
2023-06-03 18:02:36 -0500

    Update RaicesMultiples.py

--- Backend/metodos/RaicesMultiples.py
+++ Backend/metodos/RaicesMultiples.py
@@ -1,10 +1,10 @@
-
-def RaicesMultiples(f, df, d2f, x0, tol, Nmax):
+def RaicesMultiples(f, df, d2f, x0, tol, Nmax, Err):
     # Inicialización
     xant = x0
     fant = f(xant)
     E = 1000
     cont = 0
+    errores = []  # Lista para almacenar los errores
     
     # Ciclo
     while E > tol and cont < Nmax:
@@ -14,11 +14,16 @@ def RaicesMultiples(f, df, d2f, x0, tol, Nmax):
         cont = cont + 1
         xant = xact
         fant = fact
-    
+        errores.append(E)  # Agregar el error a la lista
+        
     # Entrega de resultados
     x = xact
     iteraciones = cont
-    err = E
     
-    return ({"x":x,"Iter":iteraciones, "Error":err})
-
+    if Err == 1:  # Error absoluto
+        return {"x": x, "Iter": iteraciones, "Errores": errores}
+    elif Err == 2:  # Error relativo
+        errores_relativos = [e / abs(x) for e in errores]  # Calcular errores relativos
+        return {"x": x, "Iter": iteraciones, "Errores": errores_relativos}
+    else:
+        return "Invalid value for the 'Err' parameter. Please use 1 for relative error or 2 for absolute error."
