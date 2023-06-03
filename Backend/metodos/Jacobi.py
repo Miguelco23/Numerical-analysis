6059b80b7b1f94e0ac9fd819cd705fea360214f6
Gustavo-Lopez13 <46933074+Gustavo-Lopez13@users.noreply.github.com>
2023-06-03 16:48:12 -0500

    Update Jacobi.py

--- Backend/metodos/Jacobi.py
+++ Backend/metodos/Jacobi.py
@@ -1,8 +1,8 @@
 import numpy as np
 
-def Jacobi(A_, b_, x0_, tol, Nmax):
-    # Inicialización
-    A=np.array(A_)
+def Jacobi(A_, b_, x0_, tol, Nmax, Err):
+    # Initialization
+    A = np.array(A_)
     b = np.array(b_)
     x0 = np.array(x0_)
     D = np.diag(np.diag(A))
@@ -14,16 +14,29 @@ def Jacobi(A_, b_, x0_, tol, Nmax):
     E = 1000
     cont = 0
     
-    # Ciclo
+    # Iteration
     while E > tol and cont < Nmax:
         xact = T.dot(xant) + C
         E = np.linalg.norm(xant - xact)
         xant = xact
         cont += 1
-    
-    # Entrega de resultados
-    x = xant
-    iterations = cont
-    err = E
 
-    return ({"x":np.array2string(x),"iterations":iterations,"Error":err})
+    if Err == 1:
+       # Calculate absolute error
+       err_abs = np.linalg.norm(b - A.dot(xact))
+       return {
+          "x": np.array2string(xact),
+          "iterations": cont,
+          "absolute_error": err_abs,
+       }
+    elif Err == 2:
+       # Calculate relative error
+       err_abs = np.linalg.norm(b - A.dot(xact))
+       err_rel = err_abs / np.linalg.norm(b)
+       return {
+          "x": np.array2string(xact),
+          "iterations": cont,
+          "relative_error": err_rel,
+       }
+    else:
+       return "Invalid Error parameter value. Please use 1 for relative error or 2 for absolute error."
