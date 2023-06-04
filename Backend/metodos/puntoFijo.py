d68d9e322a90d3b2a8e2b5042f0c83bc06ec9460
Gustavo-Lopez13 <46933074+Gustavo-Lopez13@users.noreply.github.com>
2023-06-03 19:13:05 -0500

    Update puntoFijo.py

--- Backend/metodos/puntoFijo.py
+++ Backend/metodos/puntoFijo.py
@@ -1,26 +1,34 @@
 from math import *
-import numpy as np
 
-def puntoFijo(f, g, x0, tol, nmax):
+def puntoFijo(f, g, x0, tol, nmax, Err):
+    if Err == 1:
+        error_type = "relative error"
+    elif Err == 2:
+        error_type = "absolute error"
+    else:
+        raise ValueError("Invalid value for the Err parameter. It should be 1 (absolute error) or 2 (relative error).")
+
     iter = 0
-    Err = 9999
     xant = x0
     flambda = lambda x: eval(f)
     glambda = lambda x: eval(g)
 
-    while Err> tol and iter< nmax:
+    while iter < nmax:
+        xant_ant = xant
         xact = glambda(xant)
-        Err  = abs(xant-xact)
-        print(Err)
-        iter+=1
+        Err = abs(xant - xact)
+        err_abs = abs(xact - xant)
+        err_rel = abs((xact - xant) / xant)
+        iter += 1
         xant = xact
 
+    if Err == 1:
+        print(f"Error relativo = {err_rel}")
+    elif Err == 2:
+        print(f"Error absoluto = {err_abs}")
 
-    print(f"x= {xact}")
+    print(f"x = {xact}")
     print(f"iters = {iter}")
-    print(f"Error = {Err}")
+    print(f"Error = {Err} ({error_type})")
 
-    return({"x":xact,"iters":iter,"Error":Err})
-
-# Ejemplo de como utilizar
-# puntofijo("(e**-x)-x","(e**-x)",1,5*10**(-6),12)
+    return {"x": xact, "iters": iter, "Error": Err}
