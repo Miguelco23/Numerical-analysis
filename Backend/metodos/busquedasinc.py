e325c272cefd0805641b25cab249ad0a1903eed8
Gustavo-Lopez13 <46933074+Gustavo-Lopez13@users.noreply.github.com>
2023-06-03 19:01:57 -0500

    Update busquedasinc.py

--- Backend/metodos/busquedasinc.py
+++ Backend/metodos/busquedasinc.py
@@ -1,23 +1,37 @@
 from math import *
 import numpy as np
 
-def busqueda_inc(f, x0, h, nmax):
+def incremental_search(f, x0, h, nmax, Err):
+    if Err == 1:
+        show_absolute_error = True
+        show_relative_error = False
+    elif Err == 2:
+        show_absolute_error = False
+        show_relative_error = True
+    else:
+        return "Invalid value for the Err parameter. It should be 1 (absolute error) or 2 (relative error)."
+
     xant = x0
     flambda = lambda x: eval(f)
     fant = flambda(xant)
-    xact = xant+h
+    xact = xant + h
     fact = flambda(xact)
     for i in range(nmax):
+        absolute_error = abs(xact - xant)
+        relative_error = abs((xact - xant) / xact) * 100
+
+        if show_absolute_error:
+            print(f"Absolute error: {absolute_error}")
+        if show_relative_error:
+            print(f"Relative error (%): {relative_error}")
+
         print(f"{fant} {fact}")
-        if fant * fact <0:
-            # print(f"hay raiz entre {xant} y {xact}")
-            # print(f"iters = {i}")
-            return(f"Root is between {xant} and {xact}. output with {i} iterations") # Retorna dos limites de el intervalo y las iteraciones
+        if fant * fact < 0:
+            return f"Root is between {xant} and {xact}. Output with {i} iterations"
+
         xant = xact
         fant = fact
-        xact = xant+h
+        xact = xant + h
         fact = flambda(xact)
 
-
-# Ejemplo de como utilizar
-# busqueda_inc("(x**3)+3*x+2",-2,0.0075,1000)
+    return "No root found within the given interval."
