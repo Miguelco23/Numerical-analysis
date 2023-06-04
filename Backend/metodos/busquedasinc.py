0c1d9edd32582087a97e27855ba9ebce8871069c
Gustavo-Lopez13 <46933074+Gustavo-Lopez13@users.noreply.github.com>
2023-06-03 19:11:27 -0500

    Update busquedasinc.py

--- Backend/metodos/busquedasinc.py
+++ Backend/metodos/busquedasinc.py
@@ -1,37 +1,34 @@
 from math import *
-import numpy as np
 
-def incremental_search(f, x0, h, nmax, Err):
+def puntoFijo(f, g, x0, tol, nmax, Err):
     if Err == 1:
-        show_absolute_error = True
-        show_relative_error = False
+        error_type = "relative error"
     elif Err == 2:
-        show_absolute_error = False
-        show_relative_error = True
+        error_type = "absolute error"
     else:
-        return "Invalid value for the Err parameter. It should be 1 (absolute error) or 2 (relative error)."
+        raise ValueError("Invalid value for the Err parameter. It should be 1 (absolute error) or 2 (relative error).")
 
+    iter = 0
     xant = x0
     flambda = lambda x: eval(f)
-    fant = flambda(xant)
-    xact = xant + h
-    fact = flambda(xact)
-    for i in range(nmax):
-        absolute_error = abs(xact - xant)
-        relative_error = abs((xact - xant) / xact) * 100
+    glambda = lambda x: eval(g)
 
-        if show_absolute_error:
-            print(f"Absolute error: {absolute_error}")
-        if show_relative_error:
-            print(f"Relative error (%): {relative_error}")
-
-        print(f"{fant} {fact}")
-        if fant * fact < 0:
-            return f"Root is between {xant} and {xact}. Output with {i} iterations"
-
+    while iter < nmax:
+        xant_ant = xant
+        xact = glambda(xant)
+        Err = abs(xant - xact)
+        err_abs = abs(xact - xant)
+        err_rel = abs((xact - xant) / xant)
+        iter += 1
         xant = xact
-        fant = fact
-        xact = xant + h
-        fact = flambda(xact)
 
-    return "No root found within the given interval."
+    if Err == 1:
+        print(f"Error relativo = {err_rel}")
+    elif Err == 2:
+        print(f"Error absoluto = {err_abs}")
+
+    print(f"x = {xact}")
+    print(f"iters = {iter}")
+    print(f"Error = {Err} ({error_type})")
+
+    return {"x": xact, "iters": iter, "Error": Err}
