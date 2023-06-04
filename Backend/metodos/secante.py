b61dcbde93a2b654eece197a1049acfb5852e8fe
Gustavo-Lopez13 <46933074+Gustavo-Lopez13@users.noreply.github.com>
2023-06-03 19:31:03 -0500

    Update secante.py

--- Backend/metodos/secante.py
+++ Backend/metodos/secante.py
@@ -1,4 +1,7 @@
-def secante(f, x0, x1, tol, max_iter):
+def secante(f, x0, x1, tol, max_iter, err):
+    if err != 1 and err != 2:
+        return "Invalid value for the Err parameter. It should be 1 (absolute error) or 2 (relative error)."
+
     i = 0
     while i < max_iter:
         try:
@@ -10,20 +13,15 @@ def secante(f, x0, x1, tol, max_iter):
             x1 = x1 - fx1 * dx
             fx1 = eval(f.replace('x', str(x1)))
             if abs(fx1) <= tol:
-                return("The root of the function is: "+ str(x1))
+                return "The root of the function is: " + str(x1)
+            if i > 0:
+                if err == 1:
+                    error = abs(fx1)
+                    print("Iteration %d - Absolute Error: %f" % (i, error))
+                elif err == 2:
+                    error = abs((x1 - x0) / x1)
+                    print("Iteration %d - Relative Error: %f" % (i, error))
             i += 1
         except ZeroDivisionError:
-            return("Error: Division by zero.")
-    return("The method does not converge after %d iterations." % max_iter)
-
-
-# intervalo y toleracia
-# f = "x**2 - 2"
-# x0 = 1
-# x1 = 2
-# tol = 1e-6
-# max_iter = 5
-
-# raiz = secante(f, x0, x1, tol, max_iter)
-# print(raiz)
-
+            return "Error: Division by zero."
+    return "The method does not converge after %d iterations." % max_iter
